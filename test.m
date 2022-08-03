259d25663c4fc5d1a1457d92a8158884f31d479a
Ajay Singh <info2azooba@gmail.com>
2022-08-03 14:22:51 +0545

    test updated

--- test.m
+++ test.m
@@ -2,10 +2,15 @@ clear all;
 close all;
 clc;
 
-xn = 2*cos(pi/2)*sin(pi/2)
+% xn = 2*cos(pi/2)*sin(pi/2)
 
-dft = fft(xn)
+% dft = fft(xn)
 
-xk = [1 2 3 4 5 6 0 0]
+% xk = [1 2 3 4 5 6 0 0]
 
-idft = fft(xk)
+% idft = fft(xk)
+
+xn = [1 2 3]
+hn = [1 2 1 -1]
+
+yn = conv(xn, hn)
